5cb267641d2ec6a81d0676f47fb569898d592c7c
nuchs <sjbrown@live.co.uk>
2025-09-19 16:03:50 +0100

    Complete tutorials 1-3

---
+++ draw.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/gdamore/tcell/v2"
+
+func DrawRune(s tcell.Screen, x, y int, r rune, st tcell.Style) {
+	s.SetContent(x, y, r, nil, st)
+}
+
+func DrawString(s tcell.Screen, x, y int, str string, st tcell.Style) {
+	col := x
+	for _, r := range str {
+		s.SetContent(col, y, r, nil, st)
+		col++
+	}
+}
+
+func DrawBox(s tcell.Screen, x1, y1, x2, y2 int, st tcell.Style) {
+	for col := x1; col < x2; col++ {
+		DrawRune(s, col, y1, '─', st)
+		DrawRune(s, col, y2, '─', st)
+	}
+
+	for row := y1; row < y2; row++ {
+		DrawRune(s, x1, row, '│', st)
+		DrawRune(s, x2, row, '│', st)
+	}
+
+	DrawRune(s, x1, y1, '┌', st)
+	DrawRune(s, x1, y2, '└', st)
+	DrawRune(s, x2, y1, '┐', st)
+	DrawRune(s, x2, y2, '┘', st)
+}
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"unicode/utf8"
+
+	"github.com/gdamore/tcell/v2"
 )
 
 func main() {
@@ -14,4 +19,90 @@ func main() {
 	}
 
 	log.Printf("Snake config -> %s", cfg)
+
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "creating screen: ", err)
+		os.Exit(1)
+	}
+	if err = screen.Init(); err != nil {
+		fmt.Fprintln(os.Stderr, "initialising screen: ", err)
+		os.Exit(1)
+	}
+	defer safeFini(screen)
+
+	base := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	screen.SetStyle(base)
+	screen.Clear()
+
+	msg := "Hello! tcell - press ESC or Ctrl-C"
+	drawCentered(screen, msg, base)
+	screen.Show()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		screen.PostEvent((tcell.NewEventInterrupt(nil)))
+	}()
+
+	for {
+		ev := screen.PollEvent()
+		switch e := ev.(type) {
+		case *tcell.EventKey:
+			switch e.Key() {
+			case tcell.KeyEscape, tcell.KeyCtrlC:
+				return
+			default:
+				w, h := screen.Size()
+				status := fmt.Sprintf("Last key: %v (rune=%q) - Esc/Ctrl+C to quit", e.Key(), e.Rune())
+				for x := range w {
+					screen.SetContent(x, h-1, ' ', nil, base)
+				}
+				drawAt := func(col, row int, s string, st tcell.Style) {
+					for i, r := range s {
+						if col+i >= w {
+							break
+						}
+						screen.SetContent(col+i, row, r, nil, st)
+					}
+				}
+				drawAt(0, h-1, status, base.Dim(true))
+				screen.Show()
+			}
+		case *tcell.EventResize:
+			screen.Clear()
+			drawCentered(screen, msg, base)
+			screen.Sync()
+			screen.Show()
+
+		case *tcell.EventInterrupt:
+			return
+		}
+	}
+}
+
+func safeFini(s tcell.Screen) {
+	if s == nil {
+		return
+	}
+
+	defer s.Fini()
+	if r := recover(); r != nil {
+		fmt.Fprintln(os.Stderr, "panic:", r)
+	}
+}
+
+func drawCentered(s tcell.Screen, msg string, st tcell.Style) {
+	w, h := s.Size()
+	if w <= 0 || h <= 0 {
+		return
+	}
+	DrawBox(s, 0, 0, w-1, h-1, st.Bold(true))
+
+	row := h / 2
+	runeCount := utf8.RuneCountInString(msg)
+	startCol := max((w-runeCount)/2, 0)
+
+	DrawString(s, startCol, row, msg, st)
 }
