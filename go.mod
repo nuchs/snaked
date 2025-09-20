a1ff0ffd868625bd37d2078d51691f3c6d7364f8
nuchs <sjbrown@live.co.uk>
2025-09-20 21:07:19 +0100

    Completed tut4

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/nuchs/snaked
 
 go 1.25.1
 
+require github.com/gdamore/tcell/v2 v2.9.0
+
 require (
 	github.com/gdamore/encoding v1.0.1 // indirect
-	github.com/gdamore/tcell/v2 v2.9.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
--- main.go
+++ main.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
-	"syscall"
-	"unicode/utf8"
 
 	"github.com/gdamore/tcell/v2"
 )
 
+var direction = "up"
+
 func main() {
-	cfg, err := FromFlags()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Invalid config:", err)
-		os.Exit(2)
+	// cfg, err := FromFlags()
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, "Invalid config:", err)
+	// 	os.Exit(2)
+	// }
+
+	screen := createScreen()
+	defer safeFini(screen)
+
+	base := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	screen.SetStyle(base)
+	screen.Clear()
+	screen.Show()
+
+loop:
+	for {
+		ev := screen.PollEvent()
+		switch e := ev.(type) {
+		case *tcell.EventKey:
+			if handlekey(e) {
+				break loop
+			}
+			redraw(screen, base)
+		case *tcell.EventResize:
+			redraw(screen, base)
+		case *tcell.EventInterrupt:
+			break loop
+		}
 	}
+}
 
-	log.Printf("Snake config -> %s", cfg)
-
+func createScreen() tcell.Screen {
 	screen, err := tcell.NewScreen()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "creating screen: ", err)
@@ -29,57 +51,40 @@ func main() {
 		fmt.Fprintln(os.Stderr, "initialising screen: ", err)
 		os.Exit(1)
 	}
-	defer safeFini(screen)
 
-	base := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
-	screen.SetStyle(base)
-	screen.Clear()
+	return screen
+}
 
-	msg := "Hello! tcell - press ESC or Ctrl-C"
-	drawCentered(screen, msg, base)
-	screen.Show()
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sigCh
-		screen.PostEvent((tcell.NewEventInterrupt(nil)))
-	}()
-
-	for {
-		ev := screen.PollEvent()
-		switch e := ev.(type) {
-		case *tcell.EventKey:
-			switch e.Key() {
-			case tcell.KeyEscape, tcell.KeyCtrlC:
-				return
-			default:
-				w, h := screen.Size()
-				status := fmt.Sprintf("Last key: %v (rune=%q) - Esc/Ctrl+C to quit", e.Key(), e.Rune())
-				for x := range w {
-					screen.SetContent(x, h-1, ' ', nil, base)
-				}
-				drawAt := func(col, row int, s string, st tcell.Style) {
-					for i, r := range s {
-						if col+i >= w {
-							break
-						}
-						screen.SetContent(col+i, row, r, nil, st)
-					}
-				}
-				drawAt(0, h-1, status, base.Dim(true))
-				screen.Show()
-			}
-		case *tcell.EventResize:
-			screen.Clear()
-			drawCentered(screen, msg, base)
-			screen.Sync()
-			screen.Show()
-
-		case *tcell.EventInterrupt:
-			return
-		}
+func handlekey(key *tcell.EventKey) bool {
+	switch key.Key() {
+	case tcell.KeyEscape, tcell.KeyCtrlC:
+		return true
 	}
+
+	switch key.Rune() {
+	case 'a', 'A':
+		direction = "left"
+	case 'w', 'W':
+		direction = "up"
+	case 'd', 'D':
+		direction = "right"
+	case 's', 'S':
+		direction = "down"
+	}
+
+	return false
+}
+
+func redraw(s tcell.Screen, st tcell.Style) {
+	w, h := s.Size()
+	status := fmt.Sprintf("Direction: %s (Esc to quit)", direction)
+
+	for x := range w {
+		DrawRune(s, x, h-1, ' ', st)
+	}
+
+	DrawString(s, 0, h-1, status, st)
+	s.Show()
 }
 
 func safeFini(s tcell.Screen) {
@@ -91,18 +96,4 @@ func safeFini(s tcell.Screen) {
 	if r := recover(); r != nil {
 		fmt.Fprintln(os.Stderr, "panic:", r)
 	}
-}
-
-func drawCentered(s tcell.Screen, msg string, st tcell.Style) {
-	w, h := s.Size()
-	if w <= 0 || h <= 0 {
-		return
-	}
-	DrawBox(s, 0, 0, w-1, h-1, st.Bold(true))
-
-	row := h / 2
-	runeCount := utf8.RuneCountInString(msg)
-	startCol := max((w-runeCount)/2, 0)
-
-	DrawString(s, startCol, row, msg, st)
 }
